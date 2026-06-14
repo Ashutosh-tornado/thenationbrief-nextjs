0b9e55a1c4d3b7de284c469f0ada024bf8812f28
Ashutosh <ashutoshjr.77@gmail.com>
2026-06-14 21:41:26 +0530

    Add WordPress posts to sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -10,4 +10,32 @@ module.exports = {
       },
     ],
   },
+  additionalPaths: async (config) => {
+    const result = []
+    let page = 1
+    let hasMore = true
+
+    while (hasMore) {
+      const res = await fetch(
+        `https://cms.thenationbrief.com/wp-json/wp/v2/posts?per_page=100&page=${page}&_fields=slug,modified`
+      )
+      if (!res.ok) { hasMore = false; break }
+
+      const posts = await res.json()
+      if (posts.length === 0) { hasMore = false; break }
+
+      for (const post of posts) {
+        result.push({
+          loc: `/${post.slug}`,
+          lastmod: post.modified,
+          changefreq: 'weekly',
+          priority: 0.8,
+        })
+      }
+
+      posts.length < 100 ? hasMore = false : page++
+    }
+
+    return result
+  },
 }
